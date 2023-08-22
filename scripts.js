a633e6068037f6625c743b3531ea37d1f00bbd06
Anthony Isensee <60198052+anthonyisensee@users.noreply.github.com>
2023-08-22 13:48:07 -0400

    refactor: made object oriented

--- scripts.js
+++ scripts.js
@@ -1,96 +1,151 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     
-    document.getElementById('start_button').addEventListener('click', function() {
+    let output_pane = new OutputPane();
 
-        // create a new time period and store it in the globally available variable
-        current_time_period = new time_period;
-        current_time_period.start_time = Date.now();
-
-    });
-
-    document.getElementById('stop_button').addEventListener('click', function () {
-
-        // update the end time of the current time period and add it to time periods we're tracking
-        current_time_period.end_time = Date.now();
-        time_periods.push(current_time_period);
-        updateTotalTime();
-
-    });
-
-    document.getElementById('reset_button').addEventListener('click', function () {
-        time_periods = [];
-        current_time_period = new time_period();
-    });
-
-    // instantiates a ticker to continuously update times
-    const ms_time_interval = 100;
-    setInterval(function() {
-        updateTotalTime();
-        updateTotalMoney();
-        console.log("tick");
-    }, ms_time_interval);
-    
 });
 
-class time_period {
+class TimeTracker {
+
+    timePeriods = [];
+    currentlyTrackedTimePeriod = undefined;
     
-    constructor(start_time, end_time) {
-        this.start_time = start_time;
-        this.end_time = end_time;
+    constructor() {
+
+        // get references to buttons
+        this._startButton = document.getElementById('start_button');
+        this._pauseButton = document.getElementById('pause_button');
+        this._resetButton = document.getElementById('reset_button');
+
+        // add event listeners to buttons
+        this._startButton.addEventListener('click', () => this.start());
+        this._pauseButton.addEventListener('click', () => this.pause());
+        this._resetButton.addEventListener('click', () => this.reset());
+
     }
 
-    get total_time() {
-        return this.end_time - this.start_time;
+    get totalTrackedTime() {
+        
+        let total_time = 0;
+        this.timePeriods.forEach(timePeriod => {
+            total_time += timePeriod.totalTime;
+        });
+        
+        // include the time from the currently tracked time period if it exists
+        if (this.currentlyTrackedTimePeriod !== undefined) {
+            let currently_tracked_period = this.currentlyTrackedTimePeriod;
+            currently_tracked_period.endTime = Date.now();
+            total_time += currently_tracked_period.totalTime;
+        }
+
+        return total_time;
+
     }
-}
+    
+    start() {
+        
+        console.log("start button clicked");
+        
+        // handle button states
+        this._startButton.setAttribute('disabled', true);
+        this._pauseButton.removeAttribute('disabled');
 
-// instantiate some default variables
-let time_periods = [];
-let current_time_period = new time_period();
-let total_time_seconds = 0;
-let money_made = 0;
+        // keep track of the current time period
+        this.currentlyTrackedTimePeriod = new TimePeriod();
+        this.currentlyTrackedTimePeriod.startTime = Date.now();
 
-function timerIsRunning() {
-    const no_start = current_time_period.start_time === undefined;
-    const no_end = current_time_period.end_time === undefined;
-    return !no_start && no_end;
-}
-
-function updateTotalTime() {
-
-    let total_time = 0;
-
-    // get time from each time period already tracked
-    time_periods.forEach(function (time_period) {
-        total_time += time_period.total_time;
-    });
-
-    // also count time that is currently being tracked
-    if (timerIsRunning()) {
-        total_time += Date.now() - current_time_period.start_time;
     }
 
-    // convert milliseconds to seconds
-    total_time_seconds = Math.floor(total_time / 1000);
+    pause() {
+        
+        console.log("pause button clicked");
 
-    // get seconds, minutes, and hours
-    const seconds = total_time_seconds % 60;
-    const minutes = Math.floor(total_time_seconds / 60) % 60;
-    const hours = Math.floor(total_time_seconds / 3600);
+        // handle button states
+        this._pauseButton.setAttribute('disabled', true);
+        this._startButton.removeAttribute('disabled');
 
-    document.getElementById('total_time').innerHTML = `${hours}:${minutes}:${seconds}`;
+        // add an end time to the currently tracked time period, store it, and remove it from currently tracked
+        this.currentlyTrackedTimePeriod.endTime = Date.now();
+        this.timePeriods.push(this.currentlyTrackedTimePeriod);
+        this.currentlyTrackedTimePeriod = undefined;
+
+        console.log(this.timePeriods);
+
+    }
+
+    reset() {
+
+        console.log("reset button clicked");
+
+        // handle button states
+        this._pauseButton.setAttribute('disabled', true);
+        this._startButton.removeAttribute('disabled');
+
+        // reset the currently tracked time period
+        this.currentlyTrackedTimePeriod = undefined;
+
+        // stop the ui update ticker
+        clearInterval(this._ticker);
+
+    }
 
 }
 
-function updateTotalMoney() {
+class TimePeriod {
 
-    const yearly_salary = document.getElementById('yearly_salary').value;
-    const hours_per_week = document.getElementById('hours_per_week').value;
+    _startTime = undefined;
+    _endTime = undefined;
+    _totalTime = undefined;
 
-    money_made = yearly_salary / 52 / hours_per_week / 60 / 60 * total_time_seconds;
+    get startTime() {
+        return this._startTime;
+    }
 
-    // turn money into a string with two decimal places
-    money_made = money_made.toFixed(2);
+    set startTime(value) {
+        this._startTime = value;
+        this.calculateTotalTime();
+    }
 
-    document.getElementById('total_money').innerHTML = money_made;
+    get endTime() {
+        return this._endTime;
+    }
+
+    set endTime(value) {
+        this._endTime = value;
+        this.calculateTotalTime();
+    }
+
+    get totalTime() {
+        return this._totalTime;
+    }
+
+    calculateTotalTime() {
+        this._totalTime = this._endTime - this._startTime;
+    }
+
+}
+
+class OutputPane {
+    
+    _timeTracker = new TimeTracker();
+    _totalTime = undefined;
+    _totalMoney = undefined;
+    
+    _tickerInterval = 100;  // in milliseconds
+    _ticker = setInterval(() => {
+        console.log("updating dashboard");
+        this.updateTotalTime();
+    }, this._tickerInterval);
+    
+    constructor() {
+        this._totalTimeElement = document.getElementById('total_time');
+        this._totalMoneyElement = document.getElementById('total_money');
+    }
+
+    updateTotalTime(totalTime) {
+        this._totalTimeElement.innerHTML = this._timeTracker.totalTrackedTime;
+    }
+
+    updateTotalMoney(totalMoney) {
+        this._totalMoneyElement.innerHTML = totalMoney;
+    }
 }
